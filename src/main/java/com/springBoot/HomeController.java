d9c342dda62d5b92a2d2197d4c7063a133d4f581
Anusha <anusha.mreddy2@gmail.com>
2019-02-03 23:55:35 +0530

    Spring boot App

---
+++ src/main/java/com/springBoot/HomeController.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.springBoot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class HomeController {
+	
+	
+	@RequestMapping("home")
+	
+	public String getHome() {
+		System.out.println("Hiiii");
+		return "home.jsp";
+		
+	}
+
+}
